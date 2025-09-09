a3befdd7ee68a1eb3c0e739a0949af166e061a1d
Latif <latif.hensbergen@icloud.com>
2025-09-09 11:40:53 +0200

    feat: spa router

--- panel/src/main.js
+++ panel/src/main.js
@@ -1,1 +1,18 @@
+import Router from "@/framework/router";
+
 import "./style.css";
+
+import HomePage from "@/pages/index.html?raw";
+
+
+/**
+ * @type {Array<{path: string, document: string}>}
+ */
+const ROUTES = [
+    {
+        path: "/",
+        document: HomePage
+    }
+];
+
+new Router(ROUTES, {fallback: "/"});
---
+++ panel/vite.config.js
@@ -0,0 +1,14 @@
+import { defineConfig } from "vite";
+import { resolve } from "path";
+
+export default defineConfig({
+    server: {
+        historyApiFallback: true
+    },
+    resolve: {
+        alias: [{
+            find: "@",
+            replacement: resolve(__dirname, "./src")
+        }]
+    }
+});
