0e13014d9822462f26df9981be0c7c4038f34b33
Nubojay <Jason.longayroux@gmail.com>
2025-09-26 10:59:06 +0200

    Basis javascript, kleine aanpassingen

---
+++ frontend/src/dashboard.js
@@ -0,0 +1,58 @@
+// Dummy data
+const dashboardData = {
+    reservations: 18,
+    availability: ["12 plekken", "2 accommodaties"],
+    messages: "12 ongelezen berichten",
+    catering: ["Frikandelbroodjes : 2", "Stokbrood : 25"],
+    invoice: "Volgende factuurdatum xx/xx/xxxx \n €420",
+    turnover: "€420"
+  };
+  
+  //Reserveringen
+  {
+
+  }
+  
+  // BEschikbaarheid
+  {
+    const ul = document.createElement("ul");
+    dashboardData.availability.forEach(item => {
+      const li = document.createElement("li");
+      li.textContent = item;
+      ul.appendChild(li);
+    });
+    document.getElementById("availabillity").appendChild(ul);
+  }
+  
+  //Berichten
+  {
+    const p = document.createElement("p");
+    p.textContent = dashboardData.messages;
+    document.getElementById("messages").appendChild(p);
+  }
+  
+  //Catering
+  {
+    const ul = document.createElement("ul");
+    dashboardData.catering.forEach(item => {
+      const li = document.createElement("li");
+      li.textContent = item;
+      ul.appendChild(li);
+    });
+    document.getElementById("catering").appendChild(ul);
+  }
+  
+  //Factuur
+  {
+    const p = document.createElement("p");
+    p.textContent = dashboardData.invoice;
+    document.getElementById("invoice").appendChild(p);
+  }
+  
+  //Omzet
+  {
+    const p = document.createElement("p");
+    p.textContent = dashboardData.turnover;
+    document.getElementById("turnover").appendChild(p);
+  }
+  
