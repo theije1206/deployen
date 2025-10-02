543a244b8d4c6eb180811a295ae047673e5ed080
Kennethkennethkenneth <kenneth.cheung@student.hu.nl>
2025-10-02 13:41:38 +0200

    Renamed fies + changed the linking in the files

--- frontend/src/main.js
+++ frontend/src/main.js
@@ -3,12 +3,19 @@ import '@/styles/navigation.css';
 import '@/styles/typography.css';
 import '@/styles/heading.css';
 import '@/styles/button.css';
-
+import '@/styles/notifications.css';
+import '@/styles/sanitair.css';
 
 import Router from "@/framework/router";
 
 import HomePage from "@/pages/index.html?raw";
 import AdminHomePage from "@/pages/admin/home/index.html?raw";
+import sanitairPage from "@/pages/sanitair/index.html?raw";
+import hygienePage from'@/pages/sanitair/hygiene.html?raw';
+import defectPage from '@/pages/sanitair/defect.html?raw';
+import allNotificationsPage from '@/pages/sanitair/all-notifications.html?raw';
+import storagePage from '@/pages/sanitair/storage.html?raw';
+import elsePage from '@/pages/sanitair/else.html?raw';
 
 /**
  * @type {Array<{path: string, document: string}>}
@@ -21,6 +28,30 @@ const ROUTES = [
     {
         path: "/admin",
         document: AdminHomePage
+    },
+    {
+        path: "/sanitair",
+        document: sanitairPage
+    },
+    {
+        path: "/sanitair/hygiene",
+        document: hygienePage
+    },
+    {
+        path: "/sanitair/defect",
+        document: defectPage
+    },
+    {
+        path: "/sanitair/all-notifications",
+        document: allNotificationsPage
+    },
+    {
+        path: "/sanitair/storage",
+        document: storagePage
+    },
+    {
+        path: "/sanitair/else",
+        document: elsePage
     }
 ];
 
